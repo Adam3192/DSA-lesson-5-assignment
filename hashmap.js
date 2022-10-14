86819199d62f264c501ef940c4ea722f2b1d91ce
Adam3192 <jeremyadixon@hotmail.com>
2022-10-14 14:54:07 -0500

    hashmap

--- hashmap.js
+++ hashmap.js
@@ -1,3 +1,9 @@
+// The time complexity for my function will be O(1) and the space complexity will be O(n).
+
+// Using a hash map is more efficient than using an array because it has a much faster search ability 
+// than an array, time complexity on lookup is constant O(1) , but the array would be slower with a 
+// time complexity of O(n).
+
 const words = new Map()
 
 const regex = /[! "#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g
@@ -9,13 +15,15 @@ function compareStrings(stringA, stringB) {
   words.set('string1', word1)
   words.set('string2', word2)
 
-  if (words.get('string1') == '') {
+  if (words.get('string1') == '' && words.get('string2') !== '') {
     console.log('No')
   } else if (words.get('string2').includes(words.get('string1'))) {
     console.log('Yes')
   } else {
     console.log('No')
   }
+
+  console.log(words.entries());
 }
 
 compareStrings('ello', 'hello, friend!')
--- hashmap2.js
+++ hashmap2.js
@@ -1,17 +1,13 @@
-// Create a basic Hash table
 class HashTable {
  constructor() {
    this.values = {}; //store key-value pairs in its index
    this.length = 0 // length of hash table
  }
 
-  // Let's create a simple hash function that determines the key based on a characteristic of the string, such as the length. 
-  
-  // Simple CalculateHash function that determines the key based on length of string
  calculateHash(key){
     return key.toString().length;
   }
-  
+
 put(key, value) {
     const hash = this.calculateHash(key);
   // Checks if hash value exists or not:
@@ -35,30 +31,5 @@ put(key, value) {
   }
 }
 
-//create object of type hash table
 const hash_table = new HashTable();
-//add data to the hash table
 hash_table.put("EEE", "6")
-hash_table.put("FFF", "9")
-hash_table.put("A", "2")
-hash_table.put("C", "4")
-hash_table.put("BB", "1")
-hash_table.put("DD", "6")
-
-console.log(hash_table)
-// Let's try searching for a string in the hash table
-console.log("Search for the string A:")
-console.log(hash_table.get("A"))
-
-/* The output:
-HashTable {
-  values: {
-    '1': { A: '2', C: '4' },
-    '2': { BB: '1', DD: '6' },
-    '3': { EEE: '6', FFF: '9' }
-  },
-  length: 6
-}
-Search for the string A:
-2
-*/
