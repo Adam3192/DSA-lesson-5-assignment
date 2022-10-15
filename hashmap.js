67bbcd5fb2b0359814685c7053b195f2e544dccb
Adam3192 <jeremyadixon@hotmail.com>
2022-10-15 11:43:43 -0500

    hashmap

--- hashmap.js
+++ hashmap.js
@@ -6,28 +6,32 @@
 
 const words = new Map()
 
-const regex = /[! "#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g
+const punctuation = /[! "#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g
 
 function compareStrings(stringA, stringB) {
-  let word1 = stringA.replace(regex, '')
-  let word2 = stringB.replace(regex, '')
+  let word1 = stringA.replace(punctuation, '')
+  let word2 = stringB.replace(punctuation, '')
 
-  words.set('string1', word1)
-  words.set('string2', word2)
-
-  if (words.get('string1') == '' && words.get('string2') !== '') {
-    console.log('No')
-  } else if (words.get('string2').includes(words.get('string1'))) {
-    console.log('Yes')
-  } else {
-    console.log('No')
-  }
+  words.set('string1', word1);
+  words.set('string2', word2);
 
   console.log(words.entries());
+
+  if (words.get('string1') == '' && words.get('string2') !== '') {
+    console.log('No');
+    console.log('');
+  } else if (words.get('string2').includes(words.get('string1'))) {
+    console.log('Yes');
+    console.log('');
+  } else {
+    console.log('No');
+    console.log('');
+  }
+
 }
 
-compareStrings('ello', 'hello, friend!')
-compareStrings(', ', 'hello, friend!')
-compareStrings('hello friend', 'hello, friend!')
-compareStrings('lofri', 'hello, friend!')
-compareStrings('hello, friend!', 'hello')
+compareStrings('ello', 'hello, friend!');
+compareStrings(', ', 'hello, friend!');
+compareStrings('hello friend', 'hello, friend!');
+compareStrings('lofri', 'hello, friend!');
+compareStrings('hello, friend!', 'hello');
